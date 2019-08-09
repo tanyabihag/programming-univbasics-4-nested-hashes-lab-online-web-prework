0a7e26bc5a6238ee9b96408a799cc4212a83c176
Tanya Bihag <tbihag@gmail.com>
2019-08-09 04:03:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,10 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
+	programmer_hash = :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
  		{
         :grace_hopper => {
           :known_for => "COBOL",
